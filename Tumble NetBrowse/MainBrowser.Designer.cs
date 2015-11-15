c977dbafa307dd422a00c9f3204d1800c0d8387f
tumblegamer <tumble@tumblegamer.tk>
2015-11-15 14:46:46 +0000

    Upload files to GitHub

---
+++ Tumble NetBrowse/MainBrowser.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace Tumble_NetBrowse
+{
+    partial class tumblenet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(tumblenet));
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.menuBar = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.urlBox = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.backButton = new System.Windows.Forms.Button();
+            this.forwardButton = new System.Windows.Forms.Button();
+            this.sepeator1 = new System.Windows.Forms.Label();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.refreshButon = new System.Windows.Forms.Button();
+            this.homeButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.aboutTumbleNetBrowseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuBar.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser.Location = new System.Drawing.Point(0, 53);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.ScrollBarsEnabled = false;
+            this.webBrowser.Size = new System.Drawing.Size(801, 418);
+            this.webBrowser.TabIndex = 0;
+            this.webBrowser.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.webBrowser_Navigated);
+            // 
+            // menuBar
+            // 
+            this.menuBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.toolsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuBar.Location = new System.Drawing.Point(0, 0);
+            this.menuBar.Name = "menuBar";
+            this.menuBar.Size = new System.Drawing.Size(801, 24);
+            this.menuBar.TabIndex = 1;
+            this.menuBar.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.exitToolStripMenuItem.ShortcutKeyDisplayString = "";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutTumbleNetBrowseToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // urlBox
+            // 
+            this.urlBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.urlBox.Location = new System.Drawing.Point(256, 27);
+            this.urlBox.Name = "urlBox";
+            this.urlBox.Size = new System.Drawing.Size(533, 20);
+            this.urlBox.TabIndex = 2;
+            this.urlBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.urlBox_KeyDown);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar,
+            this.toolStripStatusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 474);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(801, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusBar";
+            // 
+            // toolStripProgressBar
+            // 
+            this.toolStripProgressBar.Name = "toolStripProgressBar";
+            this.toolStripProgressBar.Size = new System.Drawing.Size(100, 16);
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.AutoToolTip = true;
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(44, 17);
+            this.toolStripStatusLabel.Text = "dsfsfsd";
+            // 
+            // backButton
+            // 
+            this.backButton.AutoSize = true;
+            this.backButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.backButton.Location = new System.Drawing.Point(12, 24);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(23, 23);
+            this.backButton.TabIndex = 4;
+            this.backButton.Text = "<";
+            this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
+            // forwardButton
+            // 
+            this.forwardButton.AutoSize = true;
+            this.forwardButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.forwardButton.Location = new System.Drawing.Point(41, 24);
+            this.forwardButton.Name = "forwardButton";
+            this.forwardButton.Size = new System.Drawing.Size(23, 23);
+            this.forwardButton.TabIndex = 4;
+            this.forwardButton.Text = ">";
+            this.forwardButton.UseVisualStyleBackColor = true;
+            this.forwardButton.Click += new System.EventHandler(this.forwardButton_Click);
+            // 
+            // sepeator1
+            // 
+            this.sepeator1.AutoSize = true;
+            this.sepeator1.Location = new System.Drawing.Point(70, 30);
+            this.sepeator1.Name = "sepeator1";
+            this.sepeator1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.sepeator1.Size = new System.Drawing.Size(9, 13);
+            this.sepeator1.TabIndex = 5;
+            this.sepeator1.Text = "|";
+            // 
+            // stopButton
+            // 
+            this.stopButton.AutoSize = true;
+            this.stopButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.stopButton.Location = new System.Drawing.Point(85, 24);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(39, 23);
+            this.stopButton.TabIndex = 6;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // refreshButon
+            // 
+            this.refreshButon.AutoSize = true;
+            this.refreshButon.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.refreshButon.Location = new System.Drawing.Point(130, 24);
+            this.refreshButon.Name = "refreshButon";
+            this.refreshButon.Size = new System.Drawing.Size(54, 23);
+            this.refreshButon.TabIndex = 6;
+            this.refreshButon.Text = "Refresh";
+            this.refreshButon.UseVisualStyleBackColor = true;
+            this.refreshButon.Click += new System.EventHandler(this.refreshButon_Click);
+            // 
+            // homeButton
+            // 
+            this.homeButton.AutoSize = true;
+            this.homeButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.homeButton.Location = new System.Drawing.Point(205, 25);
+            this.homeButton.Name = "homeButton";
+            this.homeButton.Size = new System.Drawing.Size(45, 23);
+            this.homeButton.TabIndex = 7;
+            this.homeButton.Text = "Home";
+            this.homeButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(190, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(9, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "|";
+            // 
+            // aboutTumbleNetBrowseToolStripMenuItem
+            // 
+            this.aboutTumbleNetBrowseToolStripMenuItem.Name = "aboutTumbleNetBrowseToolStripMenuItem";
+            this.aboutTumbleNetBrowseToolStripMenuItem.Size = new System.Drawing.Size(211, 22);
+            this.aboutTumbleNetBrowseToolStripMenuItem.Text = "About Tumble NetBrowse";
+            this.aboutTumbleNetBrowseToolStripMenuItem.Click += new System.EventHandler(this.aboutTumbleNetBrowseToolStripMenuItem_Click);
+            // 
+            // tumblenet
+            // 
+            this.AccessibleDescription = "Browse the Internet!!";
+            this.AccessibleName = "Tumble NetBrowser";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(801, 496);
+            this.Controls.Add(this.homeButton);
+            this.Controls.Add(this.refreshButon);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sepeator1);
+            this.Controls.Add(this.forwardButton);
+            this.Controls.Add(this.backButton);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.urlBox);
+            this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.menuBar);
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.menuBar;
+            this.Name = "tumblenet";
+            this.Text = "Tumble NetBrowse";
+            this.menuBar.ResumeLayout(false);
+            this.menuBar.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.MenuStrip menuBar;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TextBox urlBox;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Button forwardButton;
+        private System.Windows.Forms.Label sepeator1;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Button refreshButon;
+        private System.Windows.Forms.Button homeButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutTumbleNetBrowseToolStripMenuItem;
+    }
+}
+
---
+++ Tumble NetBrowse/MainBrowser.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tumble_NetBrowse
+{
+    public partial class tumblenet : Form
+    {
+        public tumblenet()
+        {
+            InitializeComponent();
+            webBrowser.DocumentTitleChanged +=
+            new EventHandler(webBrowser_DocumentTitleChanged);
+            webBrowser.StatusTextChanged +=
+            new EventHandler(webBrowser_StatusTextChanged);
+
+            webBrowser.GoHome();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Navigate(String address)
+        {
+            if (String.IsNullOrEmpty(address)) return;
+            if (address.Equals("about:blank")) return;
+            if (!address.StartsWith("http://") &&
+                !address.StartsWith("https://"))
+            {
+                address = "http://" + address;
+            }
+            try
+            {
+                webBrowser.Navigate(new Uri(address));
+            }
+            catch (System.UriFormatException)
+            {
+                return;
+            }
+        }
+
+        private void webBrowser_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            urlBox.Text = webBrowser.Url.ToString();
+        }
+
+        private void urlBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Navigate(urlBox.Text);
+            }
+        }
+
+        private void webBrowser_DocumentTitleChanged(object sender, EventArgs e)
+        {
+            if (webBrowser.DocumentTitle != "") {
+                this.Text = webBrowser.DocumentTitle + " - Tumble NetBrowse";
+            }
+        }
+
+        private void webBrowser_StatusTextChanged(object sender, EventArgs e)
+        {
+            toolStripStatusLabel.Text = webBrowser.StatusText;
+        }
+
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            webBrowser.GoBack();
+        }
+
+        private void forwardButton_Click(object sender, EventArgs e)
+        {
+            webBrowser.GoForward();
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            webBrowser.Stop();
+        }
+
+        private void refreshButon_Click(object sender, EventArgs e)
+        {
+            if (!webBrowser.Url.Equals("about:blank"))
+            {
+                webBrowser.Refresh();
+            }
+        }
+
+        private void homeButton_Click(object sender, EventArgs e)
+        {
+            webBrowser.GoHome();
+        }
+
+        private void aboutTumbleNetBrowseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            new About().Show();
+        }
+    }
+}
